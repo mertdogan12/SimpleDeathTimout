1f4dcb510447df1a9ba48c0313b0d21ee1928619
Mert Dogan <mert.dogan@me.com>
2021-07-02 17:32:05 +0200

    Main created

---
+++ src/main/java/at/dogan/simpleDeathTimeout/SimpleDeathTimeout.java
@@ -0,0 +1,22 @@
+package at.dogan.simpleDeathTimeout;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Plugin main class
+ */
+public class SimpleDeathTimeout extends JavaPlugin {
+    /**
+     * Prefix from the Plugin
+     */
+    public static final String PREFIX = "§f[§1S§cD§6T§f] ";
+
+    @Override
+    public void onEnable() {
+        ConsoleCommandSender sender = Bukkit.getConsoleSender();
+
+        sender.sendMessage(PREFIX + "§6Plugin enabled");
+    }
+}
